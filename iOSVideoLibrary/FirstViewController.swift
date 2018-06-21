cba8aa3b15d6cf2eea35a8d4a6496adc5060b87a
William Rory Kronmiller <wrk961@gmail.com>
2018-06-21 07:41:18 -0400

    Initial Commit

---
+++ iOSVideoLibrary/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iOSVideoLibrary
+//
+//  Created by William Rory Kronmiller on 6/21/18.
+//  Copyright © 2018 William Rory Kronmiller. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iOSVideoLibrary/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iOSVideoLibrary
+//
+//  Created by William Rory Kronmiller on 6/21/18.
+//  Copyright © 2018 William Rory Kronmiller. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
