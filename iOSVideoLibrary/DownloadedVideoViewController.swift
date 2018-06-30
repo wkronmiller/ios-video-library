7a7b94092341ded31122f82b14a6f83f4de788c1
William Rory Kronmiller <wrk961@gmail.com>
2018-06-30 11:26:07 -0400

    Debug downloaded video view

--- iOSVideoLibrary/DownloadedVideoViewController.swift
+++ iOSVideoLibrary/DownloadedVideoViewController.swift
@@ -11,11 +11,10 @@ import UIKit
 import AVKit
 
 class DownloadedVideosViewController: UICollectionViewController {
-    private var refreshControl: UIRefreshControl!
     private var videoCategories: VideoCategories?
     
-    @objc private func updateVideoList() {
-        VideoLibrary.shared.listVideos(refresh: true).done { categories in
+    private func updateVideoList() {
+        VideoLibrary.shared.listVideos(refresh: false).done { categories in
             self.videoCategories = categories
             self.collectionView?.reloadData()
         }
@@ -23,13 +22,12 @@ class DownloadedVideosViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.refreshControl = UIRefreshControl()
-        self.refreshControl.addTarget(self, action: #selector(updateVideoList), for: .valueChanged)
-        self.collectionView?.addSubview(refreshControl)
-        VideoLibrary.shared.listVideos(refresh: false).done { categories in
-            self.videoCategories = categories
-            self.collectionView?.reloadData()
-        }
+        self.updateVideoList()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.updateVideoList()
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -49,7 +47,9 @@ class DownloadedVideosViewController: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "videoPreview", for: indexPath) as! VideoPreviewCell
         if(indexPath.section == 0) {
-            cell.displayContent(videoOverview: (self.videoCategories?.youtube[indexPath.row])!)
+            cell.displayContent(videoOverview: (self.videoCategories?.youtube.filter{video in
+                return VideoLibrary.shared.videoIsCached(videoId: video.videoId)
+                }[indexPath.row])!)
             NSLog("Configured cell \(cell)")
         }
         NSLog("Returning cell \(cell) for \(indexPath)")
@@ -58,20 +58,15 @@ class DownloadedVideosViewController: UICollectionViewController {
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if(indexPath.section == 0) {
-            let video = self.videoCategories?.youtube[indexPath.row]
-            let cached = VideoLibrary.shared.videoIsCached(videoId: video!.videoId)
-            if(cached) {
-                let url = VideoLibrary.shared.getDownloadPath(videoId: video!.videoId)
-                let player = AVPlayer(url: url)
-                let controller = AVPlayerViewController()
-                controller.player = player
-                present(controller, animated: true) {
-                    player.play()
-                }
-            } else {
-                VideoLibrary.shared.syncVideos(videos: [video!]).done { _ in
-                    collectionView.reloadData()
-                }
+            let video = self.videoCategories?.youtube.filter{video in
+                return VideoLibrary.shared.videoIsCached(videoId: video.videoId)
+                }[indexPath.row]
+            let url = VideoLibrary.shared.getDownloadPath(videoId: video!.videoId)
+            let player = AVPlayer(url: url)
+            let controller = AVPlayerViewController()
+            controller.player = player
+            present(controller, animated: true) {
+                player.play()
             }
         }
     }
